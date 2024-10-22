388e3184c90224d8e758b521602106b473315952
AlenOrbanic <aorbanic@student.unipu.hr>
2024-10-22 10:00:16 +0200

    gotovo

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const express= require("express");
 let app=express();
 
 app.get("/", (req, res)=> {
-    res.send("hello world")
-})
+    res.sendFile(__dirname + '/public/express.html');})
+
+
+    app.get("/about", (req, res)=> {
+        res.sendFile(__dirname + '/public/express2.html');})
+
+
+        app.get("/users", (req, res)=> {
+            res.json(__dirname + '/public/users.json');})
+
+
+
+
+
 
 const port=3001;
 app.listen(port, (error) => {
---
+++ public/users.js
@@ -0,0 +1,5 @@
+const users = [
+    { id: 1, ime: 'Marko', prezime: 'Marković' },
+    { id: 2, ime: 'Ana', prezime: 'Anić' },
+    { id: 3, ime: 'Petar', prezime: 'Petrović' }
+];
